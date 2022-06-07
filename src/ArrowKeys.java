81c3a5a10bc8769d82b25d0aa056e5bf2a1dbf94
DarkDestroyer888 <davidshim0331@gmail.com>
2022-06-06 23:10:01 -0400

    Updated panic room

--- src/ArrowKeys.java
+++ src/ArrowKeys.java
@@ -1,8 +1,8 @@
 /**
  *David Shim
- * Version #2
- * June 3 2022
- * 7 hours
+ * Version #3
+ * June 6 2022
+ * 6 hours
  */
 
 import javax.swing.*;
@@ -13,48 +13,50 @@ import java.util.*;
 /**
  * This class uses determines the number of times an arrow key is used
  */
-public class ArrowKeys /*implements ActionListener*/ {
+public class ArrowKeys implements ActionListener {
     int screenNum = 1;
-    //JButton object1,object2,object3,object4,object5,object6,object7,object8,object9,object10;
-    int objectInteract = 0;
-    int strikes = 0;
+    static int objectInteract = 0;
+    static int strikes = 3;
     /**
      * This method uses JFrames to create a model of our actual game frame and displays the arrow keys being used
      */
+
+    JFrame frame = new JFrame();
+
+
+    JLabel bg1 = new background("roomPanel1.png",0,0,950,650);
+    JLabel bg2 = new background("roomPanel2.png",0,0,950,650);
+    JLabel bg3 = new background("roomPanel3.png",0,0,950,650);
+    JLabel bg4 = new background("roomPanel4.png",0,0,950,650);
+    JButton object1 = new plainButton(new ImageIcon("distraction1.png"),0,315,608,3269);
+    JButton object2 = new plainButton(new ImageIcon("distraction2.png"),618,320,322,340);
+    JButton object3 = new plainButton(new ImageIcon("distraction3.png"),20,0,534,270);
+    JButton object4 = new plainButton(new ImageIcon("distraction4.png"),530,290,380,323);
+    JButton object5 = new plainButton(new ImageIcon("distraction5.png"),700,40,219,240);
+    JButton object6 = new plainButton(new ImageIcon("distraction6.png"),60,290,380,321);
+    JButton object7 = new plainButton(new ImageIcon("distraction7.png"),340,160,611,441);
+    JButton object8 = new plainButton(new ImageIcon("distraction8.png"),511,168,370,93);
+    JButton object9 = new plainButton(new ImageIcon("distraction9.png"),475,38,423,103);
+    JButton object10 = new plainButton(new ImageIcon("distraction10.png"),59,26,403,524);
+    JLabel bMessage1 = new background("badMessage1.png",0,0,950,650);
+    JLabel bMessage2 = new background("badMessage2.png",0,0,950,650);
+    JLabel bMessage3 = new background("badMessage3.png",0,0,950,650);
+    JLabel bMessage4 = new background("badMessage4.png",0,0,950,650);
+    JLabel bMessage5 = new background("badMessage5.png",0,0,950,650);
+    JLabel bMessage6 = new background("badMessage6.png",0,0,950,650);
+    JLabel message1 = new background("message1.png",0,0,950,650);
+    JLabel message2 = new background("message2.png",0,0,950,650);
+    JLabel message3 = new background("message3.png",0,0,950,650);
+    JLabel message4 = new background("message4.png",0,0,950,650);
+
+    JPanel panel = new JPanel();
+
     public ArrowKeys() {
 
-        JFrame frame = new JFrame();
         frame.setSize(950, 650);
         frame.setFocusable(true);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel bg1 = new background("roomPanel1.png",0,0,950,650);
-        JLabel bg2 = new background("roomPanel2.png",0,0,950,650);
-        JLabel bg3 = new background("roomPanel3.png",0,0,950,650);
-        JLabel bg4 = new background("roomPanel4.png",0,0,950,650);
-        JButton object1 = new plainButton(new ImageIcon("distraction1.png"),0,315,608,3269);
-        JButton object2 = new plainButton(new ImageIcon("distraction2.png"),618,320,322,340);
-        JButton object3 = new plainButton(new ImageIcon("distraction3.png"),20,0,534,270);
-        JButton object4 = new plainButton(new ImageIcon("distraction4.png"),530,290,380,323);
-        JButton object5 = new plainButton(new ImageIcon("distraction5.png"),700,40,219,240);
-        JButton object6 = new plainButton(new ImageIcon("distraction6.png"),60,290,380,321);
-        JButton object7 = new plainButton(new ImageIcon("distraction7.png"),340,160,611,441);
-        JButton object8 = new plainButton(new ImageIcon("distraction8.png"),511,168,370,93);
-        JButton object9 = new plainButton(new ImageIcon("distraction9.png"),475,38,423,103);
-        JButton object10 = new plainButton(new ImageIcon("distraction10.png"),59,26,403,524);
-        JLabel bMessage1 = new background("badMessage1.png",0,0,950,650);
-        JLabel bMessage2 = new background("badMessage2.png",0,0,950,650);
-        JLabel bMessage3 = new background("badMessage3.png",0,0,950,650);
-        JLabel bMessage4 = new background("badMessage4.png",0,0,950,650);
-        JLabel bMessage5 = new background("badMessage5.png",0,0,950,650);
-        JLabel bMessage6 = new background("badMessage6.png",0,0,950,650);
-        JLabel message1 = new background("message1.png",0,0,950,650);
-        JLabel message2 = new background("message2.png",0,0,950,650);
-        JLabel message3 = new background("message3.png",0,0,950,650);
-        JLabel message4 = new background("message4.png",0,0,950,650);
-
-        JPanel panel = new JPanel();
 
         panel.setLayout(null);
         panel.add(object1);
@@ -107,66 +109,19 @@ public class ArrowKeys /*implements ActionListener*/ {
         message3.setVisible(false);
         message4.setVisible(false);
 
+        object1.addActionListener(this);
+        object2.addActionListener(this);
+        object3.addActionListener(this);
+        object4.addActionListener(this);
+        object5.addActionListener(this);
+        object6.addActionListener(this);
+        object7.addActionListener(this);
+        object8.addActionListener(this);
+        object9.addActionListener(this);
+        object10.addActionListener(this);
 
         frame.addKeyListener(new KeyListener() {
-            public void actionPerformed(ActionEvent e) {
-                if(e.getSource() == object1){
-                    object1.setVisible(false);
-                    message4.setVisible(true);
-                    objectInteract++;
-                }
-                else if(e.getSource() == object2){
-                    object2.setVisible(false);
-                    message3.setVisible(true);
-                    objectInteract++;
-                }
-                else if(e.getSource() == object3){
-                    object3.setVisible(false);
-                    bMessage2.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-                else if(e.getSource() == object4){
-                    object4.setVisible(false);
-                    bMessage1.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-                else if(e.getSource() == object5){
-                    object5.setVisible(false);
-                    message1.setVisible(true);
-                    objectInteract++;
-                }
-                else if(e.getSource() == object6){
-                    object6.setVisible(false);
-                    message2.setVisible(true);
-                    objectInteract++;
-                }
-                else if(e.getSource() == object7){
-                    object7.setVisible(false);
-                    bMessage3.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-                else if(e.getSource() == object8){
-                    object8.setVisible(false);
-                    bMessage4.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-                else if(e.getSource() == object9){
-                    object9.setVisible(false);
-                    bMessage6.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-                else if(e.getSource() == object10){
-                    object10.setVisible(false);
-                    bMessage5.setVisible(true);
-                    objectInteract++;
-                    strikes++;
-                }
-            }
+
             @Override
             public void keyReleased(KeyEvent e) {
 
@@ -265,7 +220,10 @@ public class ArrowKeys /*implements ActionListener*/ {
                         screenNum = 3;
                     }
                 }
-                else if(objectInteract == 7){
+                else if(objectInteract == 4){
+                    return;
+                }
+                else if(strikes == 0){
                     return;
                 }
             }
@@ -277,6 +235,117 @@ public class ArrowKeys /*implements ActionListener*/ {
 
     public static void main(String[] args) {
         new ArrowKeys();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
+        if(e.getSource() == object1){
+            object1.setVisible(false);
+            object2.setVisible(false);
+            object3.setVisible(false);
+            message4.setVisible(true);
+            objectInteract++;
+            message4.setVisible(false);
+            object1.setVisible(true);
+            object2.setVisible(true);
+            object3.setVisible(true);
+        }
+        else if(e.getSource() == object2){
+            object1.setVisible(false);
+            object2.setVisible(false);
+            object3.setVisible(false);
+            message3.setVisible(true);
+            objectInteract++;
+            message3.setVisible(false);
+            object1.setVisible(true);
+            object2.setVisible(true);
+            object3.setVisible(true);
+        }
+        else if(e.getSource() == object3){
+            object1.setVisible(false);
+            object2.setVisible(false);
+            object3.setVisible(false);
+            bMessage2.setVisible(true);
+            strikes--;
+            bMessage2.setVisible(false);
+            object1.setVisible(true);
+            object2.setVisible(true);
+            object3.setVisible(true);
+        }
+        else if(e.getSource() == object4){
+            object4.setVisible(false);
+            object5.setVisible(false);
+            object6.setVisible(false);
+            bMessage1.setVisible(true);
+            strikes--;
+            bMessage1.setVisible(false);
+            object4.setVisible(true);
+            object5.setVisible(true);
+            object6.setVisible(true);
+        }
+        else if(e.getSource() == object5){
+            object4.setVisible(false);
+            object5.setVisible(false);
+            object6.setVisible(false);
+            message1.setVisible(true);
+            objectInteract++;
+            message1.setVisible(false);
+            object4.setVisible(true);
+            object5.setVisible(true);
+            object6.setVisible(true);
+        }
+        else if(e.getSource() == object6){
+            object4.setVisible(false);
+            object5.setVisible(false);
+            object6.setVisible(false);
+            message2.setVisible(true);
+            objectInteract++;
+            message2.setVisible(false);
+            object4.setVisible(true);
+            object5.setVisible(true);
+            object6.setVisible(true);
+        }
+        else if(e.getSource() == object7){
+            object7.setVisible(false);
+            bMessage3.setVisible(true);
+            strikes--;
+            bMessage3.setVisible(false);
+            object7.setVisible(true);
+        }
+        else if(e.getSource() == object8){
+            object8.setVisible(false);
+            object9.setVisible(false);
+            object10.setVisible(false);
+            bMessage4.setVisible(true);
+            strikes--;
+            bMessage4.setVisible(false);
+            object8.setVisible(true);
+            object9.setVisible(true);
+            object10.setVisible(true);
+        }
+        else if(e.getSource() == object9){
+            object8.setVisible(false);
+            object9.setVisible(false);
+            object10.setVisible(false);
+            bMessage6.setVisible(true);
+            strikes--;
+            bMessage6.setVisible(false);
+            object8.setVisible(true);
+            object9.setVisible(true);
+            object10.setVisible(true);
+        }
+        else if(e.getSource() == object10){
+            object8.setVisible(false);
+            object9.setVisible(false);
+            object10.setVisible(false);
+            bMessage5.setVisible(true);
+            strikes--;
+            bMessage5.setVisible(false);
+            object8.setVisible(true);
+            object9.setVisible(true);
+            object10.setVisible(true);
+        }
     }
 
 }
